c349d29d3084ce64dea62ac6df7f427673a53397
nacho692 <mariotti.ignacio@gmail.com>
2019-07-07 17:10:12 -0300

    Fix when calculating egreedy policy probs

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import random
 import pickle
 import os.path
 import time
+random.seed(0)
 
 def epsilon_greedy(Q, board, action_spaces, epsilon):
     if board is None:
@@ -14,7 +15,7 @@ def epsilon_greedy(Q, board, action_spaces, epsilon):
     max_action = max(action_spaces, key=lambda a: Q.get(board, a))
     probs = list(
         map(lambda x: 1 - epsilon + epsilon / len(action_spaces)
-            if max_action == x[0] else epsilon / len(action_spaces),
+            if max_action == x else epsilon / len(action_spaces),
             action_spaces))
     return random.choices(list(action_spaces), probs).pop()
 
@@ -24,7 +25,7 @@ def q_learning(env, num_episodes, alpha, gamma=1.0, eps=1.0, eps_decay=.9999, ep
         Q = BoardSaver(env.size + 1)
     rw = 0
     new_boards = 0
-    over_1k = 5
+    over_1k = 3
     for e in range(num_episodes):
         board = env.reset()
         if not Q.contains(board):
@@ -34,7 +35,6 @@ def q_learning(env, num_episodes, alpha, gamma=1.0, eps=1.0, eps_decay=.9999, ep
 
         action = epsilon_greedy(Q, board, env.action_spaces, eps)
         done = False
-
         while not done:
             next_board, reward, done, info = env.step(action)
             rw += reward
@@ -51,14 +51,14 @@ def q_learning(env, num_episodes, alpha, gamma=1.0, eps=1.0, eps_decay=.9999, ep
             eps = max(epsmin, eps * eps_decay)
 
             old_q_value = Q.get(board, action)
-            next_expected_value = Q.get(next_board, next_action) if next_board is not None else 0
+            next_expected_value = max(
+                map(lambda a: Q.get(next_board, a), env.action_spaces)) if next_board is not None else 0
             new_q_value = old_q_value + \
                           alpha*(reward + gamma*next_expected_value - old_q_value)
             Q.define(board, action, new_q_value)
 
             board = next_board
             action = next_action
-
         if e % 100 == 0:
             avg_rw = rw/100
             print("episode: {}, reward rate: {}, new boards: {}, epsilon: {}, over_1k: {}"
@@ -67,7 +67,8 @@ def q_learning(env, num_episodes, alpha, gamma=1.0, eps=1.0, eps_decay=.9999, ep
             rw = 0
             if avg_rw > 1:
                 over_1k -= 1
-
+                if over_1k == 0:
+                    return Q
 
     return Q
 
@@ -81,13 +82,13 @@ with open('q_value_function_3x3.pickle', 'rb') as handle:
 
 print(len(q_value_function.boards))
 
-for e in range(3):
+for e in range(10):
     print(e)
     with open('q_value_function_3x3.pickle', 'rb') as handle:
         training_q_value_function = pickle.load(handle)
 
     env = DotsAndBoxes(2, DotsAndBoxesMaxIfKnownPolicy(training_q_value_function))
-    q_value_function = q_learning(env, 100000, alpha=0.05, gamma=0.95, eps=0.1, epsmin=0.01, eps_decay=.999995, Q=q_value_function)
+    q_value_function = q_learning(env, 2000, alpha=0.05, gamma=0.95, eps=0.1, epsmin=0.01, eps_decay=.999995, Q=q_value_function)
     del training_q_value_function
     with open('q_value_function_3x3.pickle', 'wb') as handle:
         pickle.dump(q_value_function, handle, protocol=pickle.HIGHEST_PROTOCOL)
